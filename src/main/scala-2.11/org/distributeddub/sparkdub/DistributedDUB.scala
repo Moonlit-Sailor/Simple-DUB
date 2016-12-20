76e3f09bc251b4bda0b4bee937e6f584e5b31aa5
Moonlit-Sailor <595604512@qq.com>
2016-12-20 22:27:35 +0800

    modify phase 2

--- src/main/scala-2.11/org/distributeddub/sparkdub/DistributedDUB.scala
+++ src/main/scala-2.11/org/distributeddub/sparkdub/DistributedDUB.scala
@@ -2,100 +2,105 @@ package org.distributeddub.sparkdub
 
 import java.io.PrintWriter
 
+import org.apache.spark.HashPartitioner
 import org.apache.spark.sql.{Dataset, SparkSession}
 import org.apache.spark.storage.StorageLevel
 
 import scala.collection.mutable
 import scala.math._
+import scala.util.Random
 
 /**
   * Created by root on 11/3/16.
   */
 
-object DistributedDUB {
+class DistributedDUB extends Serializable{
   def minimum(a: Int*) = a.min
 
-  def editDistanceSimilarityPercent(stra:String, strb:String): Double = {
-    if(stra == null && strb == null) 1
-    else if(stra == null || strb == null) 0
-    else{
+  def editDistanceSimilarityPercent(stra: String, strb: String): Double = {
+    if (stra == null && strb == null) 1
+    else if (stra == null || strb == null) 0
+    else {
       val a = stra.toLowerCase
       val b = strb.toLowerCase
       val length1 = a.length
       val length2 = b.length
-      val distance = Array.tabulate(length1+1,length2+1){(i,j)=>if(i==0) j else if(j == 0) i else 0}
-      for(i<- 1 to length1; j<- 1 to length2){
-        val d = if(a(i-1) == b(j-1)) 0 else 1 // if a ith == b jth, then don't need to modify the last character
-        distance(i)(j) = minimum(distance(i)(j-1)+1, distance(i-1)(j)+1, distance(i-1)(j-1)+d)
+      val distance = Array.tabulate(length1 + 1, length2 + 1) { (i, j) => if (i == 0) j else if (j == 0) i else 0 }
+      for (i <- 1 to length1; j <- 1 to length2) {
+        val d = if (a(i - 1) == b(j - 1)) 0 else 1 // if a ith == b jth, then don't need to modify the last character
+        distance(i)(j) = minimum(distance(i)(j - 1) + 1, distance(i - 1)(j) + 1, distance(i - 1)(j - 1) + d)
       } //end for
-      1- distance(length1)(length2) / max(length1,length2).toDouble
+      1 - distance(length1)(length2) / max(length1, length2).toDouble
     } //end else
   }
 
   // compute the similarity of two numbers.
   // stra, strb are numeric like string
-  def numSimilarityPercent(stra:String, strb:String): Double = {
-    val num1 = if(stra == "") 0 else stra.replace("\"", "").replace("gbp","").toDouble
-    val num2 = if(strb == "") 0 else strb.replace("\"", "").replace("gbp","").toDouble
+  def numSimilarityPercent(stra: String, strb: String): Double = {
+    val num1 = if (stra == "") 0 else stra.replace("\"", "").replace("gbp", "").toDouble
+    val num2 = if (strb == "") 0 else strb.replace("\"", "").replace("gbp", "").toDouble
 
-    if(max(num1,num2) == 0) 1 else min(num1,num2) / max(num1,num2)
+    if (max(num1, num2) == 0) 1 else min(num1, num2) / max(num1, num2)
   }
 
   //compute the square distance of two points
-  def sqdist(a:Array[Double], b:Array[Double]) :Double =
-  a.zip(b).map{case(x1,x2)=>(x1-x2)*(x1-x2)}.sum
+  def sqdist(a: Array[Double], b: Array[Double]): Double =
+  a.zip(b).map { case (x1, x2) => (x1 - x2) * (x1 - x2) }.sum
 
   // compute the density of the given point p
-//  var densityCount = 0
-  def density(p:Array[Double], points:Array[ Array[Double] ], r:Double)={
-//    densityCount += 1
-    points.count(x=>sqdist(p,x) < r*r)
+  //  var densityCount = 0
+  def density(p: Array[Double], points: Array[Array[Double]], r: Double) = {
+    //    densityCount += 1
+    points.count(x => sqdist(p, x) < r * r)
 
   }
 
 
   //transform num to its binary representation,
   //then return a list that indicates which positions contain 1
-  def onePositionSet(num: Int, pos:Int): List[Int]={
+  def onePositionSet(num: Int, pos: Int): List[Int] = {
     num match {
       case 0 => Nil
-      case x => if(x%2 == 1) pos::onePositionSet(num/2,pos+1) else onePositionSet(num/2,pos+1)
+      case x => if (x % 2 == 1) pos :: onePositionSet(num / 2, pos + 1) else onePositionSet(num / 2, pos + 1)
     }
   }
 
-//  var searchCount = 0
+  //  var searchCount = 0
   /**
     * Phase1: Binary Search for the boundary points set in virtual points region
-    * @param minPoint: minimum point of current searching region
-    * @param maxPoint: maximum point of current searching region
-    * @param k: virtual point limit defined by user.(granularity parameter)
-    * @param r: the Euler distance in the density definition
-    * @param T1: the density threshold
-    * @param points: the RDD that contains all of the record pair points
+    *
+    * @param minPoint : minimum point of current searching region
+    * @param maxPoint : maximum point of current searching region
+    * @param k        : virtual point limit defined by user.(granularity parameter)
+    * @param r        : the Euler distance in the density definition
+    * @param T1       : the density threshold
+    * @param points   : the RDD that contains all of the record pair points
     * @return Boundary Points set M
     */
-  def search(minPoint:Array[Int], maxPoint:Array[Int], k:Int, r:Double,
-             T1:Int, points:Array[Array[Double]]) : Vector[Array[Int]]={
+  def search(minPoint: Array[Int], maxPoint: Array[Int], k: Int, r: Double,
+             T1: Int, points: Array[Array[Double]]): Vector[Array[Int]] = {
 
-//    searchCount += 1
-    def searchHelper(n:Int, boundPoint:Array[Int]):Vector[Array[Int]]={
+    //    searchCount += 1
+    def searchHelper(n: Int, boundPoint: Array[Int]): Vector[Array[Int]] = {
       val min = minPoint.clone() //min point of the current space
-      val max = boundPoint map (_ -1) //max point of the current space
-      for(e<- onePositionSet(n,0)){
-        min.update(e,boundPoint(e)+1)
-        max.update(e,maxPoint(e))
+      val max = boundPoint map (_ - 1) //max point of the current space
+      for (e <- onePositionSet(n, 0)) {
+        min.update(e, boundPoint(e) + 1)
+        max.update(e, maxPoint(e))
       }
-      search(min,max,k,r,T1,points) ++ {if(n >= 2) searchHelper(n - 1, boundPoint) else Vector()}
+      search(min, max, k, r, T1, points) ++ {
+        if (n >= 2) searchHelper(n - 1, boundPoint) else Vector()
+      }
 
     } //end function searchHelper
 
     // firstly determine if there are no virtual points in current space
-    val midPoint = (minPoint zip maxPoint).map(x=>(x._1+x._2)/2)
-    val maxGreaterThanMin:Boolean = (maxPoint zip minPoint) forall(x=> x._1 >= x._2)
+    val midPoint = (minPoint zip maxPoint).map(x => (x._1 + x._2) / 2)
+    val maxGreaterThanMin: Boolean = (maxPoint zip minPoint) forall (x => x._1 >= x._2)
 
-    if(!maxGreaterThanMin || minPoint.sameElements(midPoint) || density(minPoint map (_.toDouble/k),points,r)<T1) Vector()
-    else{
-      if(density(maxPoint map (_.toDouble/k), points,r) >= T1) Vector(maxPoint)
+    if (!maxGreaterThanMin || minPoint.sameElements(midPoint) || density(minPoint map (_.toDouble / k), points, r) < T1) Vector()
+    else {
+      if (density(maxPoint map (_.toDouble / k), points, r) >= T1) Vector(maxPoint)
       else {
         val MaxBoundP = findMaxBound(minPoint, maxPoint, k, r, T1, points)
         MaxBoundP +: searchHelper(pow(2, MaxBoundP.length).toInt - 2, MaxBoundP)
@@ -103,30 +108,31 @@ object DistributedDUB {
     }
   }
 
-  def phase1Search(k:Int, r:Double, T1:Int, points:Array[Array[Double]], dim: Int)={
+  def phase1Search(k: Int, r: Double, T1: Int, points: Array[Array[Double]], dim: Int) = {
     val minPoint = new Array[Int](dim)
     val maxPoint = new Array[Int](dim)
-    for(i <- 0 until dim){
+    for (i <- 0 until dim) {
       minPoint(i) = 0
       maxPoint(i) = k
     }
-    search(minPoint, maxPoint, k, r, T1,points)
+    search(minPoint, maxPoint, k, r, T1, points)
   }
 
-  /**Find MaxPoint of the current region. The region is represented as the minimum point and the maximum point
+  /** Find MaxPoint of the current region. The region is represented as the minimum point and the maximum point
     * All virtual points between minPoint and maxPoint belong to the current region
-    * @param minPoint: the minimum point of the current region and density(minPoint) >= T1
-    * @param maxPoint: the maximum point of the current region and density(maxPoint) < T1
-    * @param k: granularity parameter
-    * @param r: the Euler distance in the density definition
-    * @param T1: the density threshold
-    * @param points: the RDD that contains all of the record pair points
+    *
+    * @param minPoint : the minimum point of the current region and density(minPoint) >= T1
+    * @param maxPoint : the maximum point of the current region and density(maxPoint) < T1
+    * @param k        : granularity parameter
+    * @param r        : the Euler distance in the density definition
+    * @param T1       : the density threshold
+    * @param points   : the RDD that contains all of the record pair points
     * @return maxPoint of the current region
     */
-  def findMaxBound(minPoint:Array[Int], maxPoint:Array[Int], k:Int, r:Double,
-                   T1:Int, points:Array[Array[Double]]) : Array[Int] ={
+  def findMaxBound(minPoint: Array[Int], maxPoint: Array[Int], k: Int, r: Double,
+                   T1: Int, points: Array[Array[Double]]): Array[Int] = {
 
-    if(density(minPoint map (_.toDouble/k), points, r)<T1) throw new Exception("There's no MaxBoundPoint in current space whose density >T1")
+    if (density(minPoint map (_.toDouble / k), points, r) < T1) throw new Exception("There's no MaxBoundPoint in current space whose density >T1")
 
     val p = minPoint.clone()
     for (i <- p.indices) {
@@ -151,87 +157,95 @@ object DistributedDUB {
     p // return p as the MaxPoint
   }
 
-  /**filter out the elements which are within the Boundary Set
-    *The remaining points are those who are not dominated by any points in the boundary set.
-    * @param pointsArray: the original points set before filter
-    * @param boundarySet: the boundary set
-    * @param k: granularity parameter
+  /** filter out the elements which are within the Boundary Set
+    * The remaining points are those who are not dominated by any points in the boundary set.
+    *
+    * @param pointsArray : the original points set before filter
+    * @param boundarySet : the boundary set
+    * @param k           : granularity parameter
     * @return : the remaining points
     */
-  def exclude(pointsArray: Array[ Array[Double] ], boundarySet:Vector[Array[Int]], k:Int)
-  : Array[ Array[Double] ] = {
-    def notDominateBy(a:Array[Double], b:Array[Double])= (a zip b).exists{case(x,y)=> x>y}
+  def exclude(pointsArray: Array[Array[Double]], boundarySet: Vector[Array[Int]], k: Int)
+  : Array[Array[Double]] = {
+    def notDominateBy(a: Array[Double], b: Array[Double]) = (a zip b).exists { case (x, y) => x > y }
 
-    pointsArray.filter(p=>boundarySet.forall(b => notDominateBy(p,b map(_.toDouble/k))))
+    pointsArray.filter(p => boundarySet.forall(b => notDominateBy(p, b map (_.toDouble / k))))
   }
 
 
-  def containsElem(c:mutable.Iterable[Array[Int]], elem:Array[Int]):Boolean={
-    for(e<-c){
-      if(e sameElements elem) return true
+  def containsElem(c: mutable.Iterable[Array[Int]], elem: Array[Int]): Boolean = {
+    for (e <- c) {
+      if (e sameElements elem) return true
     }
     false
   }
 
   def phase2Search(pointsAfterPhase1: Array[Array[Double]], phase1Boundary: Vector[Array[Int]],
-                   T1: Int, T2: Int, k: Int, r:Double) = {
+                   T1: Int, T2: Int, k: Int, r: Double) = {
     val q = new mutable.Queue[Array[Int]]()
     q ++= phase1Boundary
     val phase2BoundPoints = new mutable.Queue[Array[Int]]()
-    while(q.nonEmpty){
+    while (q.nonEmpty) {
       val current = q.dequeue()
-      for(i<- current.indices; j<- -1 to 1 by 2){
+      for (i <- current.indices; j <- -1 to 1 by 2) {
         val neighbour = current.clone()
         neighbour(i) += j
-        if(neighbour(i)>=0 && neighbour(i)<=k && !containsElem(phase2BoundPoints,neighbour)
-          && !containsElem(q,neighbour)){
-          val den = density(neighbour map (_.toDouble/k),pointsAfterPhase1,r)
-          if(den>=T2 && den<T1){
+        if (neighbour(i) >= 0 && neighbour(i) <= k && !containsElem(phase2BoundPoints, neighbour)
+          && !containsElem(q, neighbour)) {
+          val den = density(neighbour map (_.toDouble / k), pointsAfterPhase1, r)
+          if (den >= T2 && den < T1) {
             q += neighbour
             phase2BoundPoints += neighbour
           }
         } //end outer if
 
-      }//end for
-    }// end while
+      } //end for
+    } // end while
     phase2BoundPoints
   }
+}
 
+object execute {
   def main(args: Array[String]): Unit = {
-    val warehousePath = "file:///root/software/RecordMatching/data/"
 
     val dim = 4 // 4 features
     val k = 50
-    val r = 0.5
-    val T1 = 50
-    val T2 = 5000
-    val numPartitions = 16
+    val r = 0.25
+    val T1 = 250
+    val T2 = 100
+    val numPartitions = 2
 
     val ss = SparkSession.builder().master("spark://10.1.0.23:7077").appName("DistributedDUB").getOrCreate()
 
-    // points's element is type of DataSet[(String, String, Array[Double](4))]
-//    import ss.implicits._
-//    val points = ss.read.parquet(warehousePath+"points2.parquet").map(row=> (row.getString(0),
-//      row.getString(1), row.getSeq[Double](2).toArray) ).persist(StorageLevel.MEMORY_AND_DISK)
+    val randomGen = new Random(47)
 
-    val points = ss.sparkContext.textFile("hdfs://10.0.0.23:9000/sample_set").repartition(numPartitions)
+    val data = ss.sparkContext.textFile("hdfs://10.0.0.23:9000/dblp_acm")
+      .map(line => (randomGen.nextInt(1000) % numPartitions, line)).partitionBy(new HashPartitioner(numPartitions))
+
+    val points = data.map{case (num,line) => line}
       .map(line=>line.split(",").take(dim)).map(pArray=>pArray.map(_.toDouble))
+    points.persist(StorageLevel.MEMORY_AND_DISK)
 
     val totalCount = points.count
-
     println("total:"+totalCount)
 
-    val localPoints = points.mapPartitionsWithIndex( (partitionId, iter) => {
+    val dubObj = new DistributedDUB()
+    val broadcastDUB = ss.sparkContext.broadcast(dubObj)
+
+    val pointsByPartition = points.mapPartitionsWithIndex( (partitionId, iter) => {
       val pointsArray = iter.toArray
 
-      val localBoundarySet = phase1Search(k, r, T1, pointsArray, dim)
-      val pointsAfterPhase1 = exclude(pointsArray,localBoundarySet,k)
-      val phase2BoundPoints = phase2Search(pointsAfterPhase1,localBoundarySet,T1,T2,k,r)
+      val localBoundarySet = broadcastDUB.value.phase1Search(k, r, T1, pointsArray, dim)
+      val pointsAfterPhase1 = broadcastDUB.value.exclude(pointsArray,localBoundarySet,k)
+      val phase2BoundPoints = broadcastDUB.value.phase2Search(pointsAfterPhase1,localBoundarySet,T1,T2,k,r)
 
-      val result = pointsAfterPhase1.filter(p => phase2BoundPoints forall(b => sqdist(b.map(_.toDouble/k),p)>r*r))
+      val result = pointsAfterPhase1.filter(
+        p => phase2BoundPoints forall(b => broadcastDUB.value.sqdist(b.map(_.toDouble/k),p)>r*r))
       Seq((partitionId,result)).iterator
     })
 
+    pointsByPartition.foreach(t => println(t._1 + ":" + t._2.length))
+//    pointsByPartition.saveAsTextFile("hdfs://10.0.0.23:9000/resultPoints")
 
 //    //**************collect the local boundary set***************
 //    val boundarySet = points.mapPartitionsWithIndex( (partitionId, iter) => {
